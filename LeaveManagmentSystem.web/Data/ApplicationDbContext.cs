ee67e0ab80085eb95cbf8737fe5eb1802a9d2899
tomcatprime <admin@up2cloud.one>
2025-07-21 15:38:27 +0300

    changed to LeaveType.cs

--- LeaveManagmentSystem.web/Data/ApplicationDbContext.cs
+++ LeaveManagmentSystem.web/Data/ApplicationDbContext.cs
@@ -9,5 +9,7 @@ namespace LeaveManagmentSystem.web.Data
             : base(options)
         {
         }
+
+        public DbSet <LeaveType> LeaveTypes { get; set; }
     }
 }
---
+++ LeaveManagmentSystem.web/Data/LeaveType.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LeaveManagmentSystem.web.Data
+{
+    public class LeaveType
+    {
+
+        public int Id { get; set; }
+        [Column(TypeName = "nvarchar(150)")]
+        public string Name { get; set; }
+        public int NumberOfDays { get; set; }
+
+
+
+
+    }
+}
